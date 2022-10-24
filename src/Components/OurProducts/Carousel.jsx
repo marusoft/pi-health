428978e779e906b0feb6949dce4b8a9a0fc1ed4f
Spink994 <ayodeji.bakare@sourcemygadgets.com>
2022-10-24 17:19:37 +0100

    Bug fix

--- src/Components/OurProducts/Carousel.jsx
+++ src/Components/OurProducts/Carousel.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Carousel = ({ trackerCounter, product, index }) => {
+const Carousel = ({ product }) => {
   return (
     <div className="flex flex-col lg:min-w-[620px] min-w-[340px] snap-center rounded-[10px] gap-8">
       <img
