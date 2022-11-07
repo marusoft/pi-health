a44b2541724f233375b29e4fb3379d66e748098c
Spink994 <ayodeji.bakare@sourcemygadgets.com>
2022-11-07 09:29:13 +0100

    Update

--- src/Pages/Pet/index.jsx
+++ src/Pages/Pet/index.jsx
@@ -4,7 +4,6 @@ import PiHeroOne from '../../Assets/images/pet-hero-1-png.png';
 import PiProductOne from '../../Assets/images/pet-products-1-png.png';
 import PiProductTwo from '../../Assets/images/pet-products-2-png.png';
 import PiProductThree from '../../Assets/images/pet-products-3-png.png';
-import PiHeroTwo from '../../Assets/images/hero-image-05.jpg';
 import { useRef } from 'react';
 import gsap, { Elastic } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
@@ -32,7 +31,7 @@ const Products = ({ title, image, listArray, identifier, prodRef }) => (
       } -translate-y-[50px] opacity-0 w-full pt-4 overflow-hidden px-2 max-h-screen`}
     >
       <img
-        className="object-cover rounded-t-[12px] shadow-lg min-h-[40vh] min-w-full"
+        className="object-cover shadow-lg min-h-[40vh] min-w-full"
         src={image}
         alt="PiProduct"
       />
@@ -40,9 +39,9 @@ const Products = ({ title, image, listArray, identifier, prodRef }) => (
     <div
       className={`${
         'list' + identifier
-      } translate-y-[50px] opacity-0 mx-2 shadow-lg [border-top:6px_solid_white] rounded-b-[12px] bg-sky-700 p-8`}
+      } translate-y-[50px] opacity-0 mx-2 mt-8 rounded-b-[12px]`}
     >
-      <ul className="w-full px-4 list-disc text-base font-[500] flex flex-col gap-4  text-white">
+      <ul className="w-full px-4 list-disc text-base font-[500] flex flex-col gap-4">
         {listArray.map((list, index) => {
           return (
             <li key={list + index} className="font-Nunito pl-2">
@@ -66,20 +65,6 @@ const Pet = () => {
   useEffect(() => {
     if (windowWidth < 769) {
       let ctx = gsap.context(() => {
-        gsap.to('.hero_text', {
-          y: 0,
-          opacity: 1,
-          duration: 1,
-          ease: Elastic.easeOut.config(1, 0.3),
-        });
-
-        gsap.to('.hero_title', {
-          y: 0,
-          opacity: 1,
-          duration: 2,
-          ease: Elastic.easeOut.config(1, 0.3),
-        });
-
         gsap.to('.hero_image', {
           y: 0,
           opacity: 1,
@@ -150,9 +135,8 @@ const Pet = () => {
           ease: Elastic.easeOut.config(1, 0.3),
 
           scrollTrigger: {
-            trigger: `.${prodRefTwo.current.classList[0]}`, // make .panel2 the trigger
-            start: '50% bottom', // 10% of .panel2 enters the bottom of the viewport
-            // Whatever other ScrollTrigger vars you need here
+            trigger: `.${prodRefTwo.current.classList[0]}`,
+            start: '50% bottom',
           },
         });
         gsap.to(`.${prodRefTwo.current.children[2].classList[0]}`, {
@@ -162,9 +146,8 @@ const Pet = () => {
           ease: Elastic.easeOut.config(1, 0.3),
 
           scrollTrigger: {
-            trigger: `.${prodRefTwo.current.classList[0]}`, // make .panel2 the trigger
-            start: '70% bottom', // 10% of .panel2 enters the bottom of the viewport
-            // Whatever other ScrollTrigger vars you need here
+            trigger: `.${prodRefTwo.current.classList[0]}`,
+            start: '70% bottom',
           },
         });
 
@@ -175,9 +158,8 @@ const Pet = () => {
           ease: Elastic.easeOut.config(1, 0.3),
 
           scrollTrigger: {
-            trigger: `.${prodRefThree.current.classList[0]}`, // make .panel2 the trigger
-            start: '30% bottom', // 10% of .panel2 enters the bottom of the viewport
-            // Whatever other ScrollTrigger vars you need here
+            trigger: `.${prodRefThree.current.classList[0]}`,
+            start: '30% bottom',
           },
         });
         gsap.to(`.${prodRefThree.current.children[1].classList[0]}`, {
@@ -187,9 +169,8 @@ const Pet = () => {
           ease: Elastic.easeOut.config(1, 0.3),
 
           scrollTrigger: {
-            trigger: `.${prodRefThree.current.classList[0]}`, // make .panel2 the trigger
-            start: '50% bottom', // 10% of .panel2 enters the bottom of the viewport
-            // Whatever other ScrollTrigger vars you need here
+            trigger: `.${prodRefThree.current.classList[0]}`,
+            start: '50% bottom',
           },
         });
         gsap.to(`.${prodRefThree.current.children[2].classList[0]}`, {
@@ -199,9 +180,8 @@ const Pet = () => {
           ease: Elastic.easeOut.config(1, 0.3),
 
           scrollTrigger: {
-            trigger: `.${prodRefThree.current.classList[0]}`, // make .panel2 the trigger
-            start: '70% bottom', // 10% of .panel2 enters the bottom of the viewport
-            // Whatever other ScrollTrigger vars you need here
+            trigger: `.${prodRefThree.current.classList[0]}`,
+            start: '70% bottom',
           },
         });
       }, petRef);
@@ -220,32 +200,15 @@ const Pet = () => {
         </div>
       ) : (
         <main ref={petRef}>
-          <section className="px-4 relative min-h-[400px] flex-col flex justify-center items-center">
-            <img
-              className="absolute object-cover min-w-full min-h-full"
-              src={PiHeroTwo}
-              alt="Pi-hero"
-            />
-            <div className="absolute bg-black opacity-60 h-full w-full" />
-            <div className="flex z-20 items-center flex-col gap-4 justify-center">
-              <h1 className="hero_title -translate-y-[50px] opacity-0 text-4xl font-[600] text-white">
-                Pi-Pet
-              </h1>
-              <p className="hero_text translate-y-[50px] opacity-0 text-white tracking-wide font-[600] font-Nunito text-center text-[18px]">
-                Remote Monitoring <br />
-                and Decision Intelligence Tool
-              </p>
-            </div>
-          </section>
-          <div className="w-full -translate-y-16  overflow-hidden px-4 max-h-screen">
+          <div className="w-full overflow-hidden max-h-screen">
             <img
               src={PiHeroOne}
-              className="hero_image translate-y-[50px] opacity-0 object-cover [border:6px_solid_white] rounded-t-[12px] min-h-[40vh] min-w-full"
+              className="hero_image translate-y-[50px] opacity-0 object-cover min-h-[40vh] min-w-full"
               alt="Hero"
             />
           </div>
           <div className="px-2">
-            <p className="pet_definition font-[500] -translate-y-[80px] opacity-0 text-[rgb(0,0,0,0.6)] text-center font-Nunito text-base">
+            <p className="pet_definition font-[500] mt-8 -translate-y-[80px] opacity-0 text-[rgb(0,0,0,0.6)] font-Nunito text-base">
               <b>Pi-Vet</b> remote monitoring and decision intelligence tool is
               a combination of wearable devices, mobile application and data
               analytics technology deployed to provide personalized,
